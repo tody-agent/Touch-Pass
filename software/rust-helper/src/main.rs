778f670cbf342cf8016e2b6fe5d9370d59f6183f
Sociads <sociads@gmail.com>
2026-08-14 00:43:06 +0700

    feat(rust-helper): complete main entrypoint and release build optimization

--- software/rust-helper/src/main.rs
+++ software/rust-helper/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("TouchPass Rust Helper v0.1.0");
+use touchpass_helper::api::create_router;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = create_router();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("TouchPass Rust Helper listening on http://{}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
